34c9332f0625cb04f30d699e8d1c97859e6d3290
Bogdan <0966312126a@gmail.com>
2022-12-09 18:50:16 +0200

    #

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-const weather = [49,51,63]
+const weather1 = [31,29,43,58,52]
+const weather2 = [49,51,63]
 
-let printHumidities = (elem,index) =>{
-    return `${elem } + ${index}`
-}
-
-console.log(printHumidities(weather))
+const weather = [...weather1, ...weather2]
+weather.forEach((elem, index) =>{
+    console.log(`${elem}% humidity in ${index+1} days`)
+})
