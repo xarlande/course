a2ed341ce2e89dc1eb6bf8ef5abc0a52d4f24283
Bogdan <0966312126a@gmail.com>
2022-12-10 16:53:26 +0200

    #

--- script.js
+++ script.js
@@ -1,44 +1,53 @@
 const check = document.querySelector('.check'), numberInput = document.querySelector('.number-input'),
-    numberBorder = document.querySelector('.guess-message'), againGame = document.querySelector('.again'), randBorder = document.querySelector('.question'), dontScore = 1;
+    againGame = document.querySelector('.again'), randBorder = document.querySelector('.question'), dontScore = 1;
 let randNumber = Math.trunc(Math.random() * 20) + 1, score = document.querySelector('.score'),
     hightScore = document.querySelector('.highscore');
 console.log(randNumber)
+const numberBorder = (message) =>{
+    document.querySelector('.guess-message').textContent = message
+}
+const scoreDecrem = () => {
+    score.textContent--
+}
+const scoreReset = () => {
+    score.textContent = 20
+}
 
 check.addEventListener('click', ()=>{
     const valueInput =Number(numberInput.value);
     if (!valueInput) {
-        numberBorder.textContent = 'Введіть число'
+        numberBorder('Введіть число')
     }
     else if (score.textContent == dontScore){
-        numberBorder.textContent = 'У вас закінчились спроби'
+        numberBorder('У вас закінчились спроби')
         console.log(randNumber)
     }
     else if (valueInput > 20){
-        numberBorder.textContent = 'Введіть число від 1 до 20'
+        numberBorder('Введіть число від 1 до 20')
     }
     else if (valueInput <= 0){
-        numberBorder.textContent = 'Введіть число від 1 до 20'
+        numberBorder('Введіть число від 1 до 20')
     }
     else if (valueInput === randNumber) {
-        numberBorder.textContent = 'Виграш'
+        numberBorder('Виграш')
         randBorder.textContent = valueInput
         if (Number(score.textContent)>Number(hightScore.textContent)){
             hightScore.textContent = score.textContent
         }
     }
     else if (valueInput > randNumber) {
-        numberBorder.textContent = 'Введіть менше число'
-        score.textContent--
+        numberBorder('Введіть менше число')
+        scoreDecrem()
     }
     else if (valueInput < randNumber) {
-        numberBorder.textContent = 'Введіть більше число'
-        score.textContent--
+        numberBorder('Введіть більше число')
+        scoreDecrem()
     }
 })
     againGame.addEventListener('click', () => {
         randNumber = Math.trunc(Math.random()*20) + 1
-        score.textContent = 20
+        scoreReset()
         randBorder.textContent = '???'
-        numberBorder.textContent = 'Начни вгадувати'
+        numberBorder('Начни вгадувати')
         numberInput.value = ''
     })
