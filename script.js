5ae2698c021bc2ab9256799e6f004060458b0796
Bogdan <0966312126a@gmail.com>
2022-12-18 13:52:22 +0200

    #

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-'use strict';
+const score0Element = document.getElementById('score--0')
+const score1Element = document.getElementById('score--1')
+const dice = document.querySelector('.dice')
+
+dice.classList.add('hidden')
+const nullScore = (score) => {
+  score.textContent = 0;
+}
+nullScore(score0Element)
+nullScore(score1Element)
+
