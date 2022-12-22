9d23062b22aba0c81e2c989d37f8367c44ebd185
Bogdan <0966312126a@gmail.com>
2022-12-22 23:30:24 +0200

    #

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
 const score0Element = document.getElementById('score--0')
 const score1Element = document.getElementById('score--1')
-const dice = document.querySelector('.dice')
+const current0Element = document.querySelector('#current--0')
+const current1Element = document.querySelector('#current--1')
+const diceElement = document.querySelector('.dice')
+const btnNew = document.querySelector('.btn--new')
+const btnRoll = document.querySelector('.btn--roll')
+const btnHold = document.querySelector('.btn--hold')
 
-dice.classList.add('hidden')
+diceElement.classList.add('hidden')
 const nullScore = (score) => {
-  score.textContent = 0;
+  score.textContent = 0
 }
 nullScore(score0Element)
 nullScore(score1Element)
 
+let currentScore = 0
+
+btnRoll.addEventListener('click', () => {
+  const diceNumber = Math.trunc(Math.random() * 6) + 1
+  diceElement.classList.remove('hidden')
+  diceElement.src = `dice${diceNumber}.png`
+  if (diceNumber !== 1){
+    currentScore += diceNumber
+    current0Element.textContent = currentScore
+    // todo: later
+  }
+  if (diceNumber == 1){
+    currentScore = 0
+    current0Element.textContent = currentScore
+  }
+})
