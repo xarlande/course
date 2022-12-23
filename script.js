fb8eca0da8194025a11e61534ad2316734859a84
Bogdan <0966312126a@gmail.com>
2022-12-23 20:53:08 +0200

    #

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const score0Element = document.getElementById('score--0')
 const score1Element = document.getElementById('score--1')
 const current0Element = document.querySelector('#current--0')
 const current1Element = document.querySelector('#current--1')
+const player0Element = document.querySelector('.player--0')
+const player1Element = document.querySelector('.player--1')
 const diceElement = document.querySelector('.dice')
 const btnNew = document.querySelector('.btn--new')
 const btnRoll = document.querySelector('.btn--roll')
@@ -9,24 +11,35 @@ const btnHold = document.querySelector('.btn--hold')
 
 diceElement.classList.add('hidden')
 const nullScore = (score) => {
-  score.textContent = 0
+    score.textContent = 0
 }
 nullScore(score0Element)
 nullScore(score1Element)
 
+const totalScores = [0, 0]
 let currentScore = 0
+let activePlayer = 0
+const currentPlayer = document.querySelector(`.player--${activePlayer}`)
 
 btnRoll.addEventListener('click', () => {
-  const diceNumber = Math.trunc(Math.random() * 6) + 1
-  diceElement.classList.remove('hidden')
-  diceElement.src = `dice${diceNumber}.png`
-  if (diceNumber !== 1){
-    currentScore += diceNumber
-    current0Element.textContent = currentScore
-    // todo: later
-  }
-  if (diceNumber == 1){
-    currentScore = 0
-    current0Element.textContent = currentScore
-  }
+    const diceNumber = Math.trunc(Math.random() * 6) + 1
+    const currentPlayer = document.getElementById(`current--${activePlayer}`)
+    diceElement.classList.remove('hidden')
+    diceElement.src = `dice${diceNumber}.png`
+    if (diceNumber !== 1) {
+        currentScore += diceNumber
+        currentPlayer.textContent = currentScore
+    } else {
+        currentScore = 0
+        currentPlayer.textContent = currentScore
+        activePlayer == 0 ? activePlayer++ : activePlayer = 0
+        if (activePlayer == 0) {
+            player1Element.classList.remove('player--active')
+            player0Element.classList.add('player--active')
+        }
+        else  {
+            player0Element.classList.remove('player--active')
+            player1Element.classList.add('player--active')
+        }
+    }
 })
